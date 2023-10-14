b09f9395927d693b1dbe82983d58aa9e5e339905
Supriya Jaiswaal <supriyajswl43@gmail.com>
2023-10-14 17:10:01 +0530

    add files

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Resturant from "./component/basics/Resturant";
+//mport UseState from "./component/hooks/useState";
+//import UseEffect from "./component/hooks/useEffect";
+//import UseReducer from "./component/hooks/useReducer";
+//import Todo from "./component/todoreact/todo";
+// import Temp from "./component/weather/temp";
+//import Weathercard from "./component/weather/weathercard";
+const App = () => {
+  
+   return <Resturant />;
+  //return<Weathercard/>;
+};
+
+export default App;
